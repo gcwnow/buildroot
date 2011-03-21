6355bed10544859d715ca209d0968363f257c816
Maarten ter Huurne <maarten@treewalker.org>
2011-03-21 01:29:02 +0100

    pwswd: Added package.
    Power Switch Daemon for Dingoo A320/A330 and GCW Zero.
    It allows the user to change various system settings such as brightness,
    volume, TV-out and more by pressing the power slider plus a button.

---
+++ package/pwswd/pwswd.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# pwswd
+#
+#############################################################
+PWSWD_VERSION = master
+PWSWD_SITE = git://github.com/Ayla-/pwswd.git
+PWSWD_DEPENDENCIES = alsa-lib libpng
+
+define PWSWD_BUILD_CMDS
+	$(MAKE) CROSS_COMPILE="$(TARGET_CROSS)" -C $(@D)
+endef
+
+define PWSWD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/pwswd $(TARGET_DIR)/usr/sbin/pwswd
+	$(INSTALL) -D -m 0644 $(@D)/template.conf $(TARGET_DIR)/etc/pwswd.conf
+	$(INSTALL) -D -m 0755 package/pwswd/S91pwswd.sh $(TARGET_DIR)/etc/init.d/S91pwswd.sh
+endef
+
+$(eval $(generic-package))
