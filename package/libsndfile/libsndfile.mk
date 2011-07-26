23498d3e454db0c95d11c40cbbe21b22d36206bf
Maarten ter Huurne <maarten@treewalker.org>
2011-07-26 23:55:42 +0200

    libsndfile: Avoid using host version of pkg-config
    Since there seems to be no support for using a cross version of pkg-config,
    not using pkg-config at all is the best solution. This is implemented by
    feeding a non-existing path for the pkg-config to the configure script.

--- package/libsndfile/libsndfile.mk
+++ package/libsndfile/libsndfile.mk
@@ -8,4 +8,7 @@ LIBSNDFILE_VERSION = 1.0.25
 LIBSNDFILE_SITE = http://www.mega-nerd.com/libsndfile/files
 LIBSNDFILE_INSTALL_STAGING = YES
 
+# We have no cross pkg-config and using the host version can cause problems.
+LIBSNDFILE_CONF_ENV:=PKG_CONFIG=/do-not-use-pkg-config
+
 $(eval $(autotools-package))
