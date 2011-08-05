52576719b04d5c15e3a29b141f18423d34c5880e
Maarten ter Huurne <maarten@treewalker.org>
2011-08-05 04:21:18 +0200

    libao: Remove OSS plugin when ALSA is enabled.
    At runtime, libao will load all plugins, even ones that will never be used.
    Since libao will prefer ALSA over OSS by default anyway, it is pretty safe
    to exclude the OSS plugin.
    
    There is no configure option for disabling the OSS plugin, so we remove
    the shared object in a post-install hook.

--- package/libao/libao.mk
+++ package/libao/libao.mk
@@ -11,9 +11,14 @@ LIBAO_INSTALL_STAGING = YES
 LIBAO_CONF_OPT = --disable-esd --disable-wmm --disable-arts \
 			--disable-nas --disable-pulse
 
+define LIBAO_REMOVE_OSS_PLUGIN
+	rm -f $(TARGET_DIR)/usr/lib/ao/plugins-4/liboss.so
+endef
+
 ifeq ($(BR2_PACKAGE_ALSA_LIB),y)
 LIBAO_DEPENDENCIES += alsa-lib
 LIBAO_CONF_OPT += --enable-alsa --enable-alsa-mmap
+LIBAO_POST_INSTALL_TARGET_HOOKS += LIBAO_REMOVE_OSS_PLUGIN
 else
 LIBAO_CONF_OPT += --disable-alsa
 endif
