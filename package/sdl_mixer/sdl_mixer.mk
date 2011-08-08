52f73632ce27160d9da8c6f9a7546cacbb46bd3b
Maarten ter Huurne <maarten@treewalker.org>
2011-08-09 00:13:21 +0200

    sdl_mixer: Added package "timidity-instruments" for Timidity instrument data.
    Allows MIDI music to be played by SDL_mixer. This is required by some ports
    of old PC games, such as Doom 2 and Duke Nukem 3D.
    
    This commit is based on changes made by booboo for the original Dingux rootfs.
    The buildroot makefiles have been rewritten in modern style.

--- package/sdl_mixer/sdl_mixer.mk
+++ package/sdl_mixer/sdl_mixer.mk
@@ -8,14 +8,23 @@ SDL_MIXER_SOURCE:=SDL_mixer-$(SDL_MIXER_VERSION).tar.gz
 SDL_MIXER_SITE:=http://www.libsdl.org/projects/SDL_mixer/release/
 
 SDL_MIXER_INSTALL_STAGING = YES
-SDL_MIXER_DEPENDENCIES = sdl
+SDL_MIXER_DEPENDENCIES = sdl timidity-instruments
 SDL_MIXER_CONF_OPT = \
 	--without-x \
 	--with-sdl-prefix=$(STAGING_DIR)/usr \
-	--disable-music-midi \
 	--disable-music-mod \
 	--disable-music-mp3 \
 	--disable-music-flac # configure script fails when cross compiling
+
+ifeq ($(BR2_PACKAGE_TIMIDITY_INSTRUMENTS),y)
+SDL_MIXER_CONF_OPT += \
+	--enable-music-midi \
+	--enable-music-timidity-midi \
+	--disable-music-native-midi \
+	--disable-music-native-midi-gpl
+else
+SDL_MIXER_CONF_OPT += --disable-music-midi
+endif
 
 ifeq ($(BR2_PACKAGE_LIBMAD),y)
 SDL_MIXER_CONF_OPT += --enable-music-mp3-mad-gpl
