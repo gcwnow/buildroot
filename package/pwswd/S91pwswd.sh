656e3845400ecc5af06d6bef99e8af938e35e9b7
Maarten ter Huurne <maarten@treewalker.org>
2012-08-30 17:40:11 +0200

    pwswd: Make the startup script into a start/stop script
    It has to take the command into account, or it will start itself
    a second time at system shutdown.

--- package/pwswd/S91pwswd.sh
+++ package/pwswd/S91pwswd.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-HOME=`cat /etc/passwd |head -1 |cut -d':' -f 6` /usr/sbin/pwswd &
+case "$1" in
+start)
+	echo "Starting power slider daemon..."
+	export HOME=`cat /etc/passwd |head -1 |cut -d':' -f 6`
+	start-stop-daemon -S -b -x /usr/sbin/pwswd
+	unset HOME
+	;;
+stop)
+	echo "Stopping power slider daemon..."
+	start-stop-daemon -K -x /usr/sbin/pwswd
+	;;
+*)
+	echo "Usage: $0 {start|stop}"
+	exit 1
+esac
+
+exit $?
