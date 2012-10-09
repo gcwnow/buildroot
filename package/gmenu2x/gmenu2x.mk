97da4ad0b60c58497f8fdfa23c12d56ed7e9cd40
Paul Cercueil <paul@crapouillou.net>
2012-10-09 23:02:36 +0200

    gmenu2x: Build from the "packages" branch and use libopk if possible

--- package/gmenu2x/gmenu2x.mk
+++ package/gmenu2x/gmenu2x.mk
@@ -3,10 +3,14 @@
 # gmenu2x
 #
 #############################################################
-GMENU2X_VERSION = master
+GMENU2X_VERSION = packages
 GMENU2X_SITE = git://projects.qi-hardware.com/gmenu2x.git
 GMENU2X_DEPENDENCIES = sdl sdl_gfx libpng
 GMENU2X_AUTORECONF = YES
 GMENU2X_CONF_OPT = --with-sdl-prefix=$(STAGING_DIR)/usr --enable-platform=dingux
+
+ifeq ($(BR2_PACKAGE_LIBOPK),y)
+GMENU2X_DEPENDENCIES += libopk
+endif
 
 $(eval $(autotools-package))
