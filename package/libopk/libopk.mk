022fb7dfde8718941dd62ce92ee6ba404d48cd15
Paul Cercueil <paul@crapouillou.net>
2012-10-09 22:59:38 +0200

    libopk: Added package

---
+++ package/libopk/libopk.mk
@@ -0,0 +1,26 @@
+#############################################################
+#
+# libopk
+#
+#############################################################
+LIBOPK_VERSION = master
+LIBOPK_SITE = git://github.com/gcwnow/libopk.git
+LIBOPK_DEPENDENCIES = zlib
+LIBOPK_INSTALL_STAGING = YES
+
+LIBOPK_MAKE_ENV = CFLAGS="$(TARGET_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" \
+				  CROSS_COMPILE="$(TARGET_CROSS)" PREFIX=/usr
+
+define LIBOPK_BUILD_CMDS
+	$(LIBOPK_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define LIBOPK_INSTALL_STAGING_CMDS
+	$(LIBOPK_MAKE_ENV) DESTDIR="$(STAGING_DIR)" $(MAKE) -C $(@D) install
+endef
+
+define LIBOPK_INSTALL_TARGET_CMDS
+	$(LIBOPK_MAKE_ENV) DESTDIR="$(TARGET_DIR)" $(MAKE) -C $(@D) install-lib
+endef
+
+$(eval $(generic-package))
