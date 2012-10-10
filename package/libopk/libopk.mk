4da8a710399515afe2d91a1b7c5a3341abd9e7c8
Maarten ter Huurne <maarten@treewalker.org>
2012-10-10 19:45:53 +0200

    libopk: Added to index and switch to gcwnow repo
    Also use new DESTDIR/PREFIX separation that was introduced in the
    gcwnow repo.

--- package/libopk/libopk.mk
+++ package/libopk/libopk.mk
@@ -4,23 +4,23 @@
 #
 #############################################################
 LIBOPK_VERSION = master
-LIBOPK_SITE = git://github.com/Ayla-/libopk.git
+LIBOPK_SITE = git://github.com/gcwnow/libopk.git
 LIBOPK_DEPENDENCIES = zlib lzo
 LIBOPK_INSTALL_STAGING = YES
 
 LIBOPK_MAKE_ENV = CFLAGS="$(TARGET_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" \
-				  CROSS_COMPILE="$(TARGET_CROSS)"
+				  CROSS_COMPILE="$(TARGET_CROSS)" PREFIX=/usr
 
 define LIBOPK_BUILD_CMDS
 	$(LIBOPK_MAKE_ENV) $(MAKE) -C $(@D)
 endef
 
 define LIBOPK_INSTALL_STAGING_CMDS
-	$(LIBOPK_MAKE_ENV) DESTDIR="$(STAGING_DIR)/usr" $(MAKE) -C $(@D) install
+	$(LIBOPK_MAKE_ENV) DESTDIR="$(STAGING_DIR)" $(MAKE) -C $(@D) install
 endef
 
 define LIBOPK_INSTALL_TARGET_CMDS
-	$(LIBOPK_MAKE_ENV) DESTDIR="$(TARGET_DIR)/usr" $(MAKE) -C $(@D) install-lib
+	$(LIBOPK_MAKE_ENV) DESTDIR="$(TARGET_DIR)" $(MAKE) -C $(@D) install-lib
 endef
 
 $(eval $(generic-package))
