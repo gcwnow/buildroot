78375b877f89734402dced0a05524550edfb7c39
Maarten ter Huurne <maarten@treewalker.org>
2012-10-18 10:14:11 +0200

    Revert "jpeg: Go back to 6 series."
    This reverts commit 9b853bdab791ca3c2e4a49e5b1ca0c49932d7d1c.

--- package/jpeg/jpeg.mk
+++ package/jpeg/jpeg.mk
@@ -3,12 +3,10 @@
 # jpeg (libraries needed by some apps)
 #
 #############################################################
-JPEG_VERSION = 6b
-JPEG_SITE = http://mthuurne.github.com/opendingux-buildroot/distfiles/
+JPEG_VERSION = 8d
+JPEG_SITE = http://www.ijg.org/files/
 JPEG_SOURCE = jpegsrc.v$(JPEG_VERSION).tar.gz
 JPEG_INSTALL_STAGING = YES
-JPEG_LIBTOOL_PATCH = NO
-JPEG_CONF_OPT = --without-x --enable-shared --enable-static
 
 define JPEG_REMOVE_USELESS_TOOLS
 	rm -f $(addprefix $(TARGET_DIR)/usr/bin/,cjpeg djpeg jpegtrans rdjpgcom wrjpgcom)
