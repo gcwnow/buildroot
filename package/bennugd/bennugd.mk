59464318870e415efdab021f8afbe948d66adf0b
gromv <gromvrs@yahoo.fr>
2013-04-06 00:24:01 +0200

    Change bennugd repository (new version with CMake)

--- package/bennugd/bennugd.mk
+++ package/bennugd/bennugd.mk
@@ -9,22 +9,9 @@
 #BENNUGD_VERSION = 319
 
 BENNUGD_VERSION = master
-BENNUGD_SITE = git://github.com/gromv/bennugd.git
-
+BENNUGD_SITE = git://github.com/gromv/bennugd_cmake.git
+#BENNUGD_CONF_OPT = -DTARGET_LINUX
 BENNUGD_DEPENDENCIES = libpng sdl sdl_mixer openssl
+BENNUGD_INSTALL_TARGET = YES
 
-define BENNUGD_BUILD_CMDS
-        #chmod +x $(@D)/core/configure
-        #chmod +x $(@D)/modules/configure
-        #chmod +x $(@D)/tools/moddesc/configure
-        (cd $(@D); export TARGET=mipsel-gcw0-linux-uclibc; ./build-buildroot.sh release)
-endef
-
-define BENNUGD_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/bin/mipsel-gcw0-linux-uclibc/bgdi $(TARGET_DIR)/usr/bin/.
-        $(INSTALL) -D -m 0755 $(@D)/bin/mipsel-gcw0-linux-uclibc/bgdc $(TARGET_DIR)/usr/bin/.
-        $(INSTALL) -D -m 0755 $(@D)/bin/mipsel-gcw0-linux-uclibc/moddesc $(TARGET_DIR)/usr/bin/.
-        $(INSTALL) -D -m 0755 $(@D)/bin/mipsel-gcw0-linux-uclibc/*.so $(TARGET_DIR)/usr/lib/.
-endef
-
-$(eval $(generic-package))
+$(eval $(cmake-package))
