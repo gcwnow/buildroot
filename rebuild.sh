6fdb25f0f85dc4f01c1c024258d61d355e1c0592
Maarten ter Huurne <maarten@treewalker.org>
2013-04-17 22:08:48 +0200

    opendingux: Added script to do a full rebuild

---
+++ rebuild.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Get the latest version.
+echo "Updating local git repository..."
+git pull
+
+# Use the default config and patch it to point to our install location.
+make gcw0_defconfig
+sed -ie 's%^BR2_HOST_DIR=.*$%BR2_HOST_DIR="/opt/gcw0-toolchain"%' .config
+
+# Clear the install location.
+echo "Clearing install location..."
+rm -rf /opt/gcw0-toolchain
+
+# Clear the build location.
+echo "Clearing build location..."
+rm -rf output/
+
+# Remove non-versioned packages: they might have been updated since the last build.
+rm -f dl/dingux-commander-*.tar.gz
+rm -f dl/gmenu2x-*.tar.gz
+rm -f dl/libopk-*.tar.gz
+rm -f dl/libxdgmime-*.tar.gz
+rm -f dl/pyclock-*.tar.gz
+rm -f dl/pwswd-*.tar.gz
+
+# Perform the build.
+echo "Starting build..."
+make
+
+# Create packages.
+echo "Creating packages..."
+tar -C/opt --exclude='.fakeroot.*' -jcf opendingux-gcw0-toolchain.`date +'%Y-%m-%d'`.tar.bz2 gcw0-toolchain
