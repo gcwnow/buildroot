17b6fac987ad105beeecc8cba93fa332609413c6
Paul Cercueil <paul@crapouillou.net>
2013-04-27 17:48:49 -0300

    libini: Added package

---
+++ package/libini/libini.mk
@@ -0,0 +1,23 @@
+#############################################################
+#
+# libini
+#
+#############################################################
+LIBINI_VERSION = master
+LIBINI_SITE = git://github.com/pcercuei/libini.git
+LIBINI_INSTALL_STAGING = YES
+
+define LIBINI_BUILD_PYTHON
+	(cd $(@D)/python ; $(HOST_DIR)/usr/bin/python setup.py build)
+endef
+define LIBINI_INSTALL_PYTHON
+	(cd $(@D)/python ; $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
+endef
+
+ifeq ($(BR2_PACKAGE_PYTHON),y)
+	LIBINI_DEPENDENCIES += python host-python
+	LIBINI_POST_BUILD_HOOKS += LIBINI_BUILD_PYTHON
+	LIBINI_POST_INSTALL_TARGET_HOOKS += LIBINI_INSTALL_PYTHON
+endif
+
+$(eval $(cmake-package))
