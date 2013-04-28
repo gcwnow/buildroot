fb2d28872ba4a51083b51fe9ff7a53dcd88188bf
Paul Cercueil <paul@crapouillou.net>
2013-04-27 21:38:24 -0300

    pwswd: Add dependency towards libini to match upstream changes

--- package/pwswd/pwswd.mk
+++ package/pwswd/pwswd.mk
@@ -5,7 +5,7 @@
 #############################################################
 PWSWD_VERSION = master
 PWSWD_SITE = git://github.com/pcercuei/pwswd.git
-PWSWD_DEPENDENCIES = alsa-lib libpng
+PWSWD_DEPENDENCIES = alsa-lib libpng libini
 
 PWSWD_MAKE_ENV = CFLAGS="$(TARGET_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" \
 				 CROSS_COMPILE="$(TARGET_CROSS)" PREFIX=/usr \
@@ -17,7 +17,7 @@ endef
 
 define PWSWD_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/pwswd $(TARGET_DIR)/usr/sbin/pwswd
-	$(INSTALL) -D -m 0644 $(@D)/template.conf $(TARGET_DIR)/etc/pwswd.conf
+	$(INSTALL) -D -m 0644 $(@D)/pwswd.conf $(TARGET_DIR)/etc/pwswd.conf
 	$(INSTALL) -D -m 0755 package/pwswd/S91pwswd.sh $(TARGET_DIR)/etc/init.d/S91pwswd.sh
 endef
 
