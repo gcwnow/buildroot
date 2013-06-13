62059db3cdfcb55c2640eedc667ba8b76d7133d8
Paul Cercueil <paul@crapouillou.net>
2013-06-13 00:31:24 -0400

    libedje: Allow to build with LuaJIT instead of regular Lua

--- package/efl/libedje/libedje.mk
+++ package/efl/libedje/libedje.mk
@@ -9,11 +9,18 @@ LIBEDJE_SOURCE = edje-$(LIBEDJE_VERSION).tar.bz2
 LIBEDJE_SITE =  http://download.enlightenment.org/releases/
 LIBEDJE_LICENSE = GPLv2+ (epp binary), BSD-2c (everything else)
 LIBEDJE_LICENSE_FILES = COPYING
+LIBEDJE_AUTORECONF = YES
 
 LIBEDJE_INSTALL_STAGING = YES
 
-LIBEDJE_DEPENDENCIES = host-pkgconf lua libeina libeet libecore libevas \
+LIBEDJE_DEPENDENCIES = host-pkgconf libeina libeet libecore libevas \
 			libembryo
+
+ifeq ($(BR2_PACKAGE_LUAJIT),y)
+	LIBEDJE_DEPENDENCIES += luajit
+else
+	LIBEDJE_DEPENDENCIES += lua
+endif
 
 ifeq ($(BR2_PACKAGE_LIBEDJE_CC),y)
 LIBEDJE_CONF_OPT += --enable-edje-cc
