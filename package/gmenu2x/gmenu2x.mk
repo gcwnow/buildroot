1b7971cbd98c90115268031786dd9aea3ace2382
Paul Cercueil <paul@crapouillou.net>
2013-07-05 14:13:52 -0400

    gmenu2x: Add dependency towards DejaVu and SDL_ttf to match upstream

--- package/gmenu2x/gmenu2x.mk
+++ package/gmenu2x/gmenu2x.mk
@@ -5,7 +5,7 @@
 #############################################################
 GMENU2X_VERSION = packages
 GMENU2X_SITE = git://projects.qi-hardware.com/gmenu2x.git
-GMENU2X_DEPENDENCIES = sdl sdl_gfx libpng
+GMENU2X_DEPENDENCIES = sdl sdl_gfx sdl_ttf dejavu-fonts libpng
 GMENU2X_AUTORECONF = YES
 GMENU2X_CONF_OPT = --with-sdl-prefix=$(STAGING_DIR)/usr \
 				   --enable-platform=$(BR2_PACKAGE_GMENU2X_PLATFORM)
