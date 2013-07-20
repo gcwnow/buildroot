a9614e072914fe55ba680b1551b06b5840cfa3d4
Maarten ter Huurne <maarten@treewalker.org>
2013-07-20 20:13:18 +0200

    etna_viv: Added package
    User-space driver for the Vivante GCxxx series of embedded GPUs.
    
    https://github.com/laanwj/etna_viv

---
+++ package/etna_viv/etna_viv.mk
@@ -0,0 +1,36 @@
+#############################################################
+#
+# etna_viv
+#
+#############################################################
+ETNA_VIV_VERSION = master
+ETNA_VIV_SITE = git://github.com/laanwj/etna_viv.git
+ETNA_VIV_INSTALL_STAGING = YES
+# Currently, etna_viv only builds static libs, no point in installing those
+# on the target.
+ETNA_VIV_INSTALL_TARGET = NO
+
+ifeq ($(BR2_PACKAGE_ETNA_VIV_ABIV2),y)
+ETNA_VIV_ABI = v2
+else ifeq ($(BR2_PACKAGE_ETNA_VIV_ABIV4),y)
+ETNA_VIV_ABI = v4
+else
+$(error No ABI version selected)
+endif
+
+define ETNA_VIV_BUILD_CMDS
+	$(MAKE) -C $(@D)/native/etnaviv \
+		GCCPREFIX="$(TARGET_CROSS)" \
+		PLATFORM_CFLAGS="-D_POSIX_C_SOURCE=200809 -D_GNU_SOURCE -DLINUX" \
+		PLATFORM_CXXFLAGS="-D_POSIX_C_SOURCE=200809 -D_GNU_SOURCE -DLINUX" \
+		PLATFORM_LDFLAGS="-ldl -lpthread" \
+		GCABI="$(ETNA_VIV_ABI)"
+endef
+
+define ETNA_VIV_INSTALL_STAGING_CMDS
+	cp $(@D)/native/etnaviv/libetnaviv.a $(STAGING_DIR)/usr/lib
+	mkdir -p $(STAGING_DIR)/usr/include/etnaviv
+	cp $(@D)/native/etnaviv/*.h $(STAGING_DIR)/usr/include/etnaviv
+endef
+
+$(eval $(generic-package))
