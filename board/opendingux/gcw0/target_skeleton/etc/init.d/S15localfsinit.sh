6a42905dfdbe30fe7bccc32adee6267d9abd55a2
Paul Cercueil <paul@crapouillou.net>
2013-09-05 09:44:39 -0400

    opendingux: GCW0: Symlink ~/.local ~/.config to /usr/local /usr/local/etc

--- board/opendingux/gcw0/target_skeleton/etc/init.d/S15localfsinit.sh
+++ board/opendingux/gcw0/target_skeleton/etc/init.d/S15localfsinit.sh
@@ -2,10 +2,20 @@
 
 if [ -z "$1" ] || [ "x$1" = "xstart" ]; then
 
-    mkdir -p /boot/local
+    mkdir -p /usr/local
     for i in bin etc home lib sbin share; do
         mkdir -p /usr/local/$i
     done
+
+	# Symlink ~/.local to /usr/local
+	if [ ! -L /usr/local/home/.local ] ; then
+		ln -s /usr/local /usr/local/home/.local
+	fi
+
+	# Symlink ~/.config to /usr/local/etc
+	if [ ! -L /usr/local/home/.config ] ; then
+		ln -s /usr/local/etc /usr/local/home/.config
+	fi
 
     if [ ! -f /etc/local/timezone ]; then
         echo 'CET-1CEST,M3.5.0,M10.5.0' > /etc/local/timezone
