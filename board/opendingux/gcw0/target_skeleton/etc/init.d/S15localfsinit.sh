06d39e6ad9ef989ce55a80f589dd78ff3b7ede27
Maarten ter Huurne <maarten@treewalker.org>
2013-09-06 17:28:29 +0200

    opendingux: GCW0: Put shadow password file in /usr/local/etc

--- board/opendingux/gcw0/target_skeleton/etc/init.d/S15localfsinit.sh
+++ board/opendingux/gcw0/target_skeleton/etc/init.d/S15localfsinit.sh
@@ -7,6 +7,11 @@ if [ -z "$1" ] || [ "x$1" = "xstart" ]; then
         mkdir -p /usr/local/$i
     done
 
+    if [ ! -f /usr/local/etc/shadow ]; then
+        echo 'root:*:::::::' > /usr/local/etc/shadow
+        chmod 600 /usr/local/etc/shadow
+    fi
+
     if [ ! -f /usr/local/etc/timezone ]; then
         echo 'CET-1CEST,M3.5.0,M10.5.0' > /usr/local/etc/timezone
     fi
