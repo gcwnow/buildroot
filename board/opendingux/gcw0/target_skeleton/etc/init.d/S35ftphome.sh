ef3219d991e73536507b7e36dcf76e18cc05bf5d
Maarten ter Huurne <maarten@treewalker.org>
2013-09-11 11:56:25 +0200

    opendingux: GCW0: Add init script to populate ftphome
    This should have been in commit 2cd021f6.

---
+++ board/opendingux/gcw0/target_skeleton/etc/init.d/S35ftphome.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -z "$1" ] || [ "x$1" = "xstart" ]; then
+
+	mount --bind /usr/share/ftphome/media /usr/share/ftphome/media
+	mount --make-shared /media
+	mount --make-slave /usr/share/ftphome/media
+	mount --bind /media /usr/share/ftphome/media
+
+fi
