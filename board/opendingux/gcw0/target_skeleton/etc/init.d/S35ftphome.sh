b0495bb43b2c739402c4b21ce53ff2af93e6e77b
Maarten ter Huurne <maarten@treewalker.org>
2013-09-12 22:31:16 +0200

    opendingux: GCW0: Make media dir in ftphome read-only
    If the user accidentally tries to upload to the media dir, he/she will
    get an error saying the directory is not writable, instead of getting
    a disk full situation on the ramdisk that the media dir resides in.

--- board/opendingux/gcw0/target_skeleton/etc/init.d/S35ftphome.sh
+++ board/opendingux/gcw0/target_skeleton/etc/init.d/S35ftphome.sh
@@ -6,5 +6,6 @@ if [ -z "$1" ] || [ "x$1" = "xstart" ]; then
 	mount --make-shared /media
 	mount --make-slave /usr/share/ftphome/media
 	mount --bind /media /usr/share/ftphome/media
+	mount -o remount,bind,ro /media /usr/share/ftphome/media
 
 fi
