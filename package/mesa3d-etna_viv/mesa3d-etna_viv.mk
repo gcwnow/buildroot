c46157540c6c5141789a594bfd07db86041abe8e
Maarten ter Huurne <maarten@treewalker.org>
2013-10-03 18:12:16 +0200

    Freeze etna_viv and mesa3d-etna_viv versions

--- package/mesa3d-etna_viv/mesa3d-etna_viv.mk
+++ package/mesa3d-etna_viv/mesa3d-etna_viv.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-MESA3D_ETNA_VIV_VERSION = master
+MESA3D_ETNA_VIV_VERSION = 7f5833dd
 MESA3D_ETNA_VIV_SITE = git://github.com/laanwj/mesa.git
 MESA3D_ETNA_VIV_AUTORECONF = YES
 MESA3D_ETNA_VIV_INSTALL_STAGING = YES
