f9edaeae523a575690eb00fb7a167dedabee5fff
Paul Cercueil <paul@crapouillou.net>
2013-10-05 02:32:28 +0200

    libini: Switch libini.mk to use CMake to follow upstream changes

--- package/libini/libini.mk
+++ package/libini/libini.mk
@@ -7,9 +7,6 @@ LIBINI_VERSION = master
 LIBINI_SITE = git://github.com/pcercuei/libini.git
 LIBINI_INSTALL_STAGING = YES
 
-LIBINI_MAKE_ENV = CFLAGS="$(TARGET_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" \
-				  CROSS_COMPILE="$(TARGET_CROSS)" PREFIX=/usr
-
 define LIBINI_BUILD_PYTHON
 	(cd $(@D)/python ; $(HOST_DIR)/usr/bin/python setup.py build)
 endef
@@ -23,24 +20,4 @@ ifeq ($(BR2_PACKAGE_PYTHON),y)
 	LIBINI_POST_INSTALL_TARGET_HOOKS += LIBINI_INSTALL_PYTHON
 endif
 
-define LIBINI_BUILD_CMDS
-	$(LIBINI_MAKE_ENV) $(MAKE) -C $(@D)
-endef
-
-define LIBINI_INSTALL_STAGING_CMDS
-	$(LIBINI_MAKE_ENV) DESTDIR="$(STAGING_DIR)" $(MAKE) -C $(@D) install
-endef
-
-define LIBINI_INSTALL_TARGET_CMDS
-	$(LIBINI_MAKE_ENV) DESTDIR="$(TARGET_DIR)" $(MAKE) -C $(@D) install-lib
-endef
-
-define LIBINI_UNINSTALL_STAGING_CMDS
-	$(LIBINI_MAKE_ENV) DESTDIR="$(STAGING_DIR)" $(MAKE) -C $(@D) uninstall
-endef
-
-define LIBINI_UNINSTALL_TARGET_CMDS
-	$(LIBINI_MAKE_ENV) DESTDIR="$(TARGET_DIR)" $(MAKE) -C $(@D) uninstall
-endef
-
-$(eval $(generic-package))
+$(eval $(cmake-package))
