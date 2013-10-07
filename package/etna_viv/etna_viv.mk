f03e87cd2b93e7462128c246388a8515063c4f55
Wladimir J. van der Laan <laanwj@gmail.com>
2013-10-07 18:33:06 +0200

    Unfreeze etna_viv and mesa3d-etna_viv versions
    This reverts commit c46157540c6c5141789a594bfd07db86041abe8e.
    Also update the etna_viv build script to use `src` directory instead
    of `native` as this was renamed recently.

--- package/etna_viv/etna_viv.mk
+++ package/etna_viv/etna_viv.mk
@@ -3,7 +3,7 @@
 # etna_viv
 #
 #############################################################
-ETNA_VIV_VERSION = 06c1bc89
+ETNA_VIV_VERSION = master
 ETNA_VIV_SITE = git://github.com/laanwj/etna_viv.git
 ETNA_VIV_INSTALL_STAGING = YES
 # Currently, etna_viv only builds static libs, no point in installing those
@@ -19,7 +19,7 @@ $(error No ABI version selected)
 endif
 
 define ETNA_VIV_BUILD_CMDS
-	$(MAKE) -C $(@D)/native/etnaviv \
+	$(MAKE) -C $(@D)/src/etnaviv \
 		GCCPREFIX="$(TARGET_CROSS)" \
 		PLATFORM_CFLAGS="-D_POSIX_C_SOURCE=200809 -D_GNU_SOURCE -DLINUX" \
 		PLATFORM_CXXFLAGS="-D_POSIX_C_SOURCE=200809 -D_GNU_SOURCE -DLINUX" \
@@ -29,9 +29,9 @@ define ETNA_VIV_BUILD_CMDS
 endef
 
 define ETNA_VIV_INSTALL_STAGING_CMDS
-	cp $(@D)/native/etnaviv/libetnaviv.a $(STAGING_DIR)/usr/lib
+	cp $(@D)/src/etnaviv/libetnaviv.a $(STAGING_DIR)/usr/lib
 	mkdir -p $(STAGING_DIR)/usr/include/etnaviv
-	cp $(@D)/native/etnaviv/*.h $(STAGING_DIR)/usr/include/etnaviv
+	cp $(@D)/src/etnaviv/*.h $(STAGING_DIR)/usr/include/etnaviv
 endef
 
 $(eval $(generic-package))
