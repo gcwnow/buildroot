cf617ea97c15d28d52456a090eaeb392467d91f3
Paul Cercueil <paul@crapouillou.net>
2013-10-20 18:29:15 +0200

    libmikmod: Update download address to SourceForge

--- package/libmikmod/libmikmod.mk
+++ package/libmikmod/libmikmod.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 LIBMIKMOD_VERSION:=3.2.0
-LIBMIKMOD_SITE:=http://mikmod.shlomifish.org/files
+LIBMIKMOD_SITE:=http://sourceforge.net/projects/mikmod/files
 
 LIBMIKMOD_CONF_OPT = --localstatedir=/var \
 		--disable-esd
