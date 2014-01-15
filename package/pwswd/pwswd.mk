170347e6d74889c4275be9685c992bb1aec3c16f
Paul Cercueil <paul@crapouillou.net>
2014-01-15 22:40:12 +0100

    pwswd: Update to match upstream changes

--- package/pwswd/pwswd.mk
+++ package/pwswd/pwswd.mk
@@ -17,7 +17,7 @@ endef
 
 define PWSWD_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/pwswd $(TARGET_DIR)/usr/sbin/pwswd
-	$(INSTALL) -D -m 0644 $(@D)/pwswd.conf $(TARGET_DIR)/etc/pwswd.conf
+	$(INSTALL) -D -m 0644 $(@D)/pwswd-$(BR2_PACKAGE_PWSWD_PLATFORM).conf $(TARGET_DIR)/etc/pwswd.conf
 	$(INSTALL) -D -m 0755 package/pwswd/S91pwswd.sh $(TARGET_DIR)/etc/init.d/S91pwswd.sh
 endef
 
