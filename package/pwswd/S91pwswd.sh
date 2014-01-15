356cebfaf058d30a9e67ec3a6964f2a42e1dcdd5
Paul Cercueil <paul@crapouillou.net>
2014-01-15 22:42:04 +0100

    pwswd: Enhance the init script

--- package/pwswd/S91pwswd.sh
+++ package/pwswd/S91pwswd.sh
@@ -13,16 +13,30 @@ done
 case "$1" in
 start)
 	echo "Starting power slider daemon..."
-	export HOME=`cat /etc/passwd |head -1 |cut -d':' -f 6`
-	start-stop-daemon -S -b -x /usr/sbin/pwswd -- -e /dev/input/$EVENT
-	unset HOME
+	/usr/bin/env HOME=`cat /etc/passwd |head -1 |cut -d':' -f 6`	\
+		/sbin/start-stop-daemon -S -b -m -p /var/run/pwswd.pid \
+		-x /usr/sbin/pwswd -- -e /dev/input/$EVENT
 	;;
 stop)
 	echo "Stopping power slider daemon..."
-	start-stop-daemon -K -x /usr/sbin/pwswd
+	start-stop-daemon -K -q -p /var/run/pwswd.pid
+	rm -f /var/run/pwswd.pid
+	;;
+status)
+	RET=1
+	if [ -r /var/run/pwswd.pid ] ; then
+		kill -0 `cat /var/run/pwswd.pid` 2>&1 >/dev/null
+		RET=$?
+	fi
+	if [ $RET -eq 0 ] ; then
+		echo "pwswd is running"
+	else
+		echo "pwswd is NOT running"
+	fi
+	exit $RET
 	;;
 *)
-	echo "Usage: $0 {start|stop}"
+	echo "Usage: $0 {start|stop|status}"
 	exit 1
 esac
 
