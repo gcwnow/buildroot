87632c2a4c7ba3c1e7b147f9bd7acfbee32a4b7d
Maarten ter Huurne <maarten@treewalker.org>
2014-04-29 17:57:46 +0200

    opendingux: Generate ECDSA host key for dropbear
    Also don't overwrite existing keys if one key is missing.

--- board/opendingux/gcw0/target_skeleton/etc/init.d/S50dropbear-keys.sh
+++ board/opendingux/gcw0/target_skeleton/etc/init.d/S50dropbear-keys.sh
@@ -4,14 +4,25 @@
 
 RSA_KEY=/etc/dropbear/dropbear_rsa_host_key
 DSS_KEY=/etc/dropbear/dropbear_dss_host_key
+ECDSA_KEY=/etc/dropbear/dropbear_ecdsa_host_key
 
-# Exit if the keys are already present
-[ -r $RSA_KEY ] && [ -r $DSS_KEY ] && exit 0
+if [ ! -r $RSA_KEY ]
+then
+	echo -n "generating rsa key... "
+	/usr/bin/dropbearkey -t rsa -f $RSA_KEY > /dev/null 2>&1
+	echo "done"
+fi
 
-echo -n "generating rsa key... "
-/usr/bin/dropbearkey -t rsa -f $RSA_KEY > /dev/null 2>&1
-echo "done"
+if [ ! -r $DSS_KEY ]
+then
+	echo -n "generating dsa key... "
+	/usr/bin/dropbearkey -t dss -f $DSS_KEY > /dev/null 2>&1
+	echo "done"
+fi
 
-echo -n "generating dsa key... "
-/usr/bin/dropbearkey -t dss -f $DSS_KEY > /dev/null 2>&1
-echo "done"
+if [ ! -r $ECDSA_KEY ]
+then
+	echo -n "generating ecdsa key... "
+	/usr/bin/dropbearkey -t ecdsa -f $ECDSA_KEY > /dev/null 2>&1
+	echo "done"
+fi
