0d328c2f400479e48381183a71ba9efd829dd5ae
Maarten ter Huurne <maarten@treewalker.org>
2014-07-19 22:06:12 +0200

    dingux-commander: Update to match upstream config split
    This fixes the button labeling on GCW Zero, by letting the build know
    which device we're building for.

--- package/dingux-commander/dingux-commander.mk
+++ package/dingux-commander/dingux-commander.mk
@@ -8,9 +8,10 @@ DINGUX_COMMANDER_SITE = git://github.com/gcwnow/DinguxCommander.git
 DINGUX_COMMANDER_DEPENDENCIES = sdl sdl_image sdl_ttf
 
 DINGUX_COMMANDER_RESDIR = /usr/share/DinguxCommander
+DINGUX_COMMANDER_CONFIG = opendingux-$(BR2_TOOLCHAIN_BUILDROOT_VENDOR)
 
 define DINGUX_COMMANDER_BUILD_CMDS
-	$(MAKE) CXX="$(TARGET_CXX)" RESDIR="$(DINGUX_COMMANDER_RESDIR)" SDL_CONFIG="$(STAGING_DIR)/usr/bin/sdl-config" LD="$(TARGET_LD)" CONFIG=opendingux -C $(@D)
+	$(MAKE) CXX="$(TARGET_CXX)" RESDIR="$(DINGUX_COMMANDER_RESDIR)" SDL_CONFIG="$(STAGING_DIR)/usr/bin/sdl-config" LD="$(TARGET_LD)" CONFIG=$(DINGUX_COMMANDER_CONFIG) -C $(@D)
 endef
 
 ifeq ($(BR2_PACKAGE_GMENU2X),y)
@@ -21,7 +22,7 @@ endef
 endif
 
 define DINGUX_COMMANDER_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/output/opendingux/DinguxCommander $(TARGET_DIR)/usr/bin/DinguxCommander
+	$(INSTALL) -m 0755 -D $(@D)/output/$(DINGUX_COMMANDER_CONFIG)/DinguxCommander $(TARGET_DIR)/usr/bin/DinguxCommander
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/$(DINGUX_COMMANDER_RESDIR)/
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/$(DINGUX_COMMANDER_RESDIR)/ $(@D)/res/*
 	$(DINGUX_COMMANDER_INSTALL_TARGET_GMENU2X)
