96d1e206b91d4cfce134e682ec573e864580c122
Paul Cercueil <paul@crapouillou.net>
2014-07-30 16:44:56 +0200

    libmikmod: Bump to version 3.3.6

--- package/libmikmod/libmikmod.mk
+++ package/libmikmod/libmikmod.mk
@@ -3,13 +3,18 @@
 # libmikmod
 #
 #############################################################
-LIBMIKMOD_VERSION:=3.2.0
-LIBMIKMOD_SITE:=http://sourceforge.net/projects/mikmod/files
+LIBMIKMOD_VERSION:=3.3.6
+LIBMIKMOD_SITE:=http://sourceforge.net/projects/mikmod/files/libmikmod/$(LIBMIKMOD_VERSION)
 
 LIBMIKMOD_CONF_OPT = --localstatedir=/var \
 		--disable-esd
 
 LIBMIKMOD_LIBTOOL_PATCH = NO
 LIBMIKMOD_INSTALL_STAGING = YES
+
+define LIBMIKMOD_REMOVE_LIBMIKMOD_CONFIG
+rm -f $(TARGET_DIR)/usr/bin/libmikmod-config
+endef
+LIBMIKMOD_POST_INSTALL_TARGET_HOOKS += LIBMIKMOD_REMOVE_LIBMIKMOD_CONFIG
 
 $(eval $(autotools-package))
