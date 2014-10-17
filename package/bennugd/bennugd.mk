826642d3aef0f504c283992329f09ba3c81b1f3c
Paul Cercueil <paul.cercueil@analog.com>
2014-10-17 18:56:10 +0200

    bennugd: Update to use github helper macro

--- package/bennugd/bennugd.mk
+++ package/bennugd/bennugd.mk
@@ -1,15 +1,11 @@
-#############################################################
+################################################################################
 #
 # bennugd
 #
-#############################################################
-
-#BENNUGD_SITE = https://bennugd.svn.sourceforge.net/svnroot/bennugd/
-#BENNUGD_SITE_METHOD = svn
-#BENNUGD_VERSION = 319
+################################################################################
 
 BENNUGD_VERSION = master
-BENNUGD_SITE = git://github.com/gromv/bennugd_cmake.git
+BENNUGD_SITE = $(call github,gromv,bennugd_cmake,$(BENNUGD_VERSION))
 BENNUGD_DEPENDENCIES = libpng sdl sdl_mixer openssl
 BENNUGD_INSTALL_TARGET = YES
 
