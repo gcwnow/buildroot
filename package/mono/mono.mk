409a650d7aadbf307905008bff365748737f5dd5
Paul Cercueil <paul@crapouillou.net>
2014-10-19 18:57:54 +0200

    mono: Bump to version 3.10
    This patch is originally from Angelo Compagnucci, slightly patched by
    myself to fix some issues (compile without locale support, disable
    Boehm, fix installation of /etc/mono/4.5).

--- package/mono/mono.mk
+++ package/mono/mono.mk
@@ -1,45 +1,69 @@
-#############################################################
+################################################################################
 #
 # mono
 #
-#############################################################
+################################################################################
 
-#MONO_VERSION := master
-#MONO_SITE := git://github.com/mono/mono.git
-MONO_VERSION := 3.2.3
-MONO_SOURCE := mono-$(MONO_VERSION).tar.bz2
-MONO_SITE := http://download.mono-project.com/sources/mono
+MONO_VERSION = 3.10.0
+MONO_SITE = http://download.mono-project.com/sources/mono/
+MONO_SOURCE = mono-$(MONO_VERSION).tar.bz2
+MONO_LICENSE = LGPLv2 or commercial
+MONO_LICENSE_FILES = LICENSE COPYING.LIB mcs/COPYING.LIB mcs/COPYING \
+	eglib/COPYING external/Newtonsoft.Json/Tools/7-zip/copying.txt
+MONO_INSTALL_STAGING = YES
 
-MONO_DEPENDENCIES := zlib host-mono
-HOST_MONO_DEPENDENCIES := host-zlib
+## Mono native
 
-MONO_AUTORECONF := YES
-HOST_MONO_AUTORECONF := YES
+# patching configure.ac
+MONO_AUTORECONF = YES
 
-MONO_DEFAULT_CONF_OPT := --with-tls=pthread \
-	--with-mcs-docs=no --with-sgen=no
+# Disable managed code (mcs folder) from building
+MONO_CONF_OPT = --disable-gtk-doc \
+	--with-mcs-docs=no \
+	--with-moonlight=no \
+	--disable-libraries \
+	--with-ikvm-native=no \
+	--enable-minimal=aot,profiler,debug \
+	--disable-mcs-build \
+	--disable-boehm
 
-HOST_MONO_CONF_OPT := $(MONO_DEFAULT_CONF_OPT) --disable-system-aot \
-	--with-static_mono=yes --with-shared_mono=no --disable-shared \
-	--disable-mono-debugger --disable-nls --with-profile4=no --with-profile4_5=no
-
-MONO_CONF_OPT := $(MONO_DEFAULT_CONF_OPT) --disable-mcs-build \
-	--enable-minimal=aot --with-sigaltstack=no \
-	--with-static_mono=no --disable-static
-
-ifeq ($(BR2_PACKAGE_MONO),y)
-# TODO: Decide what DLLs are good to install and those which could be
-# made optional
-MONO_DLLS := mscorlib.dll System.dll System.Configuration.dll System.Core.dll \
-	System.Net.dll System.Web.dll System.Xml.dll System.Drawing.dll
-# XXX: What? $(@D) does not work?
-MONO_DLLS := $(addprefix $(BUILD_DIR)/host-mono-$(MONO_VERSION)/mcs/class/lib/net_2_0/,$(MONO_DLLS))
-
-define HOST_MONO_INSTALL_CMDS
-	mkdir -p $(TARGET_DIR)/usr/lib/mono/2.0
-	$(INSTALL) -m 0644 $(MONO_DLLS) $(TARGET_DIR)/usr/lib/mono/2.0
+define MONO_INSTALL_LIBS
+	rsync -av --exclude=*.so --exclude=*.mdb \
+		$(HOST_DIR)/usr/lib/mono $(TARGET_DIR)/usr/lib/
 endef
-endif
+
+MONO_DEPENDENCIES += host-mono
+
+MONO_POST_INSTALL_TARGET_HOOKS += MONO_INSTALL_LIBS
+
+## Mono managed
+
+HOST_MONO_CONF_OPT = --disable-gtk-doc \
+	--with-mcs-docs=no \
+	--with-moonlight=no \
+	--disable-libraries \
+	--with-ikvm-native=no \
+	--enable-minimal=aot,profiler,debug \
+	--enable-static
+
+MONO_CONF_OPT += --with-profile2=$(if $(BR2_PACKAGE_MONO_20),yes,no)
+MONO_CONF_OPT += --with-profile4=$(if $(BR2_PACKAGE_MONO_40),yes,no)
+MONO_CONF_OPT += --with-profile4_5=$(if $(BR2_PACKAGE_MONO_45),yes,no)
+
+# These options refer to the target mono, not the host. We use the host
+# only to compile libraries, then we copy them to the target.
+HOST_MONO_CONF_OPT += --with-profile2=$(if $(BR2_PACKAGE_MONO_20),yes,no)
+HOST_MONO_CONF_OPT += --with-profile4=$(if $(BR2_PACKAGE_MONO_40),yes,no)
+HOST_MONO_CONF_OPT += --with-profile4_5=$(if $(BR2_PACKAGE_MONO_45),yes,no)
+
+HOST_MONO_DEPENDENCIES = host-monolite
+
+define HOST_MONO_SETUP_MONOLITE
+	rm -rf $(@D)/mcs/class/lib/monolite
+	(cd $(@D)/mcs/class/lib; ln -s $(HOST_DIR)/usr/lib/monolite monolite)
+endef
+
+HOST_MONO_POST_CONFIGURE_HOOKS += HOST_MONO_SETUP_MONOLITE
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
