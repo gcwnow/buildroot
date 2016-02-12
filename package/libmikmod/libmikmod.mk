3ee1ce1f32f8a2c2b64ce37454088fe66c3b38a0
Dmitry Smagin <exmortis@yandex.ru>
2016-02-12 23:35:40 +0300

    libmikmod: Fixup prefix= and exec_prefix= in libmikmod-config

--- package/libmikmod/libmikmod.mk
+++ package/libmikmod/libmikmod.mk
@@ -12,9 +12,19 @@ LIBMIKMOD_CONF_OPT = --localstatedir=/var \
 LIBMIKMOD_LIBTOOL_PATCH = NO
 LIBMIKMOD_INSTALL_STAGING = YES
 
+# Fixup prefix= and exec_prefix= in libmikmod-config
+define LIBMIKMOD_FIXUP_LIBMIKMOD_CONFIG
+	$(SED) 's%prefix=/usr%prefix=$(STAGING_DIR)/usr%' \
+		$(STAGING_DIR)/usr/bin/libmikmod-config
+	$(SED) 's%exec_prefix=/usr%exec_prefix=$(STAGING_DIR)/usr%' \
+		$(STAGING_DIR)/usr/bin/libmikmod-config
+endef
+
 define LIBMIKMOD_REMOVE_LIBMIKMOD_CONFIG
 rm -f $(TARGET_DIR)/usr/bin/libmikmod-config
 endef
+
+LIBMIKMOD_POST_INSTALL_STAGING_HOOKS+=LIBMIKMOD_FIXUP_LIBMIKMOD_CONFIG
 LIBMIKMOD_POST_INSTALL_TARGET_HOOKS += LIBMIKMOD_REMOVE_LIBMIKMOD_CONFIG
 
 $(eval $(autotools-package))
